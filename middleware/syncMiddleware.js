46c8e25e28176426c438c09e2cb671e74602bf5d
shaival25 <shaivaldoshi5@gmail.com>
2024-09-25 15:23:30 +0530

    added findOne and Update hook for sync queue

--- middleware/syncMiddleware.js
+++ middleware/syncMiddleware.js
@@ -1,18 +1,31 @@
 const { addToQueue } = require("../services/syncQueue");
 
 module.exports = function (schema, atlasModelName) {
-    schema.post('save', function (doc) {
-        console.log(`Post-save hook triggered for ${atlasModelName} with document: ${doc._id}`);
-        addToQueue('update', atlasModelName, doc.toObject());
-    });
+  schema.post("save", function (doc) {
+    console.log(
+      `Post-save hook triggered for ${atlasModelName} with document: ${doc._id}`
+    );
+    addToQueue("update", atlasModelName, doc.toObject());
+  });
 
-    schema.post('remove', function (doc) {
-        console.log(`Post-remove hook triggered for ${atlasModelName} with document: ${doc._id}`);
-        addToQueue('delete', atlasModelName, doc.toObject());
-    });
+  schema.post("remove", function (doc) {
+    console.log(
+      `Post-remove hook triggered for ${atlasModelName} with document: ${doc._id}`
+    );
+    addToQueue("delete", atlasModelName, doc.toObject());
+  });
 
-    schema.post('update', function (doc) {
-        console.log(`Post-update hook triggered for ${atlasModelName} with document: ${doc._id}`);
-        addToQueue('update', atlasModelName, doc.toObject());
-    });
+  schema.post("update", function (doc) {
+    console.log(
+      `Post-update hook triggered for ${atlasModelName} with document: ${doc._id}`
+    );
+    addToQueue("update", atlasModelName, doc.toObject());
+  });
+
+  schema.post("findOneAndUpdate", function (doc) {
+    console.log(
+      `Post-update hook triggered for ${atlasModelName} with document: ${doc._id}`
+    );
+    addToQueue("update", atlasModelName, doc.toObject());
+  });
 };
