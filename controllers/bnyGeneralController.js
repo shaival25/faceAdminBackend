afd03f4b4620da96a43b573fafe88749ad4bad82
shaival25 <shaivaldoshi5@gmail.com>
2024-09-29 23:58:07 +0530

    changed api response

--- controllers/bnyGeneralController.js
+++ controllers/bnyGeneralController.js
@@ -71,7 +71,7 @@ exports.getUser = async (req, res) => {
     if (!bnyGeneral) {
       return res.status(404).json({ message: "User not found" });
     }
-    res.status(200).json(bnyGeneral);
+    res.status(200).json({ user_id: bnyGeneral._id });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
